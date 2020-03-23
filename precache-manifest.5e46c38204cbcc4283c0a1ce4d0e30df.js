53c73b32e9fa4fd5b33d6dbee6723503f5e2bfa0
pakwesi <pakwesiinfornie1@gmail.com>
2020-03-23 02:03:07 +0100

    Updates

---
+++ precache-manifest.5e46c38204cbcc4283c0a1ce4d0e30df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ba14e7fb47d0dc75bf9a4b1211e8d5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "558d076e9129e007ef94",
+    "url": "/monsters-rolodex/static/css/main.302036b7.chunk.css"
+  },
+  {
+    "revision": "df23bb5ef329776deb29",
+    "url": "/monsters-rolodex/static/js/2.4f9c44e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.4f9c44e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "558d076e9129e007ef94",
+    "url": "/monsters-rolodex/static/js/main.e192e9a5.chunk.js"
+  },
+  {
+    "revision": "9c1e41a3895b853f7042",
+    "url": "/monsters-rolodex/static/js/runtime-main.19cf4419.js"
+  }
+]);
